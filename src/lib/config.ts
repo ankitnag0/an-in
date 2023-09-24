b70c48abd7a165f193eb0bd1d47e97f8eb935c75
ankitnag <heroankit0@gmail.com>
2023-09-25 02:13:19 +0530

    Add project

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'an-in';
+export const description = 'SvelteKit blog for the absolutely nuts';
+export const url = dev ? 'http://localhost:5173/' : 'https://joyofcode.xyz/';
